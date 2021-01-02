395c31168130dcb344bdfb24c8ab21489a2b89ee
technetium-43 <58450924+technetium-43@users.noreply.github.com>
2021-01-02 09:20:36 -0600

    Removed practice problems from repo.

--- Problems/Getting input/src/Main.java
+++
@@ -1,7 +0,0 @@
-class InputClass {
-    public static String getString() {
-        // write your code here
-        java.util.Scanner userInput = new java.util.Scanner(System.in);
-        return userInput.nextLine();
-    }
-}
--- Problems/Uncomment a line/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * The class implements a simple application with the main method.
- */
-class CommentedSimpleApplication {
-
-    public static void main(String[] args) {
-        System.out.println(893_457);
-    }
-}
